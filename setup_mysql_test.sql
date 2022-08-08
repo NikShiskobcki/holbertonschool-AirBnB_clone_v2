a266ae51ccf9badb055a0f59eadb133505a34f9c
MaxiHBTN <4387@holbertonschool.com>
2022-08-08 16:21:34 +0000

    Task 4

---
+++ setup_mysql_test.sql
@@ -0,0 +1,5 @@
+-- Create database -- 
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED WITH mysql_native_password BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
