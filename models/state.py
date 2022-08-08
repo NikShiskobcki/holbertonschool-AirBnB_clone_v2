0db1644fc1e77cf8c4476c155268f8e31770f381
NikShiskobcki <nicolleshiskobcki@gmail.com>
2022-08-08 11:43:03 -0700

    updated city and state

--- models/state.py
+++ models/state.py
@@ -1,8 +1,26 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
+from os import getenv
+import models
+from models.city import City
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+    if (getenv("HBNB_TYPE_STORAGE") == "db"):
+        cities = relationship('City', backref="state")
+    else:
+        @property
+        def cities(self):
+            """list cities"""
+            from models import storage
+            mcities = []
+            for city in storage.all(City).values():
+                if city.state_id == self.id:
+                    mcities.append(city)
+            return mcities
