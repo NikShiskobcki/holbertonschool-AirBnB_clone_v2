8bfb5b97b45b2f7a97448f6af57f7d253713bd63
aldanoblezequiel <aldabaldenoblezequiel@gmail.com>
2022-08-09 13:22:47 -0700

    Class user adentro de if

--- models/user.py
+++ models/user.py
@@ -10,10 +10,15 @@ from sqlalchemy import Column, String
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
-    __tablename__ = "users"
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=True)
-    last_name = Column(String(128), nullable=True)
-    places = relationship("Place", backref="user")
-    reviews = relationship("Review", backref="user")
+    if models.storage_t =='db':
+        __tablename__ = "users"
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
+        places = relationship("Place", backref="user")
+        reviews = relationship("Review", backref="user")
+    else:
+        place_id: ""
+        user_id: ""
+        text = ""
