4d72a96fe142cd84599a57cdad5522dfdd2b5686
AxelBouvierM <axelbouvierm@gmail.com>
2022-08-09 18:55:58 +0000

    task 10

--- models/place.py
+++ models/place.py
@@ -10,11 +10,11 @@ from models.review import Review
 from os import getenv
 
 place_amenity = Table("place_amenity", Base.metadata,
-                      Column("place_id", String(60), ForeignKey("places.id", onupdate='CASCADE', ondelete='CASCADE'),
+                      Column("place_id", String(60), ForeignKey("places.id"),
                              primary_key=True, nullable=False),
                       Column("amenity_id", String(60),
-                             ForeignKey("amenities.id", onupdate='CASCADE', ondelete='CASCADE'),
-                             primary_key=True, onupdate='CASCADE', ondelete='CASCADE', nullable=False))
+                             ForeignKey("amenities.id"),
+                             primary_key=True, nullable=False))
 
 
 class Place(BaseModel, Base):
