5c6c270ab52945c88fce3fef45f8e07172da118b
aldanoblezequiel <aldabaldenoblezequiel@gmail.com>
2022-08-09 12:30:22 -0700

    Task 7 - User class update

--- models/user.py
+++ models/user.py
@@ -1,11 +1,24 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+import models
+from os import getenv
+import sqlalchemy
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, String
 
 
-class User(BaseModel):
+class User(BaseModel, Base):
     """This class defines a user by various attributes"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+    __tablename__ = "users"
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
+    places = relationship("Place", backref="user")
+    reviews = relationship("Review", backref="user")
+
+
+def __init__(self, *args, **kwargs):
+    """Init class USSER"""
+    super.().__init__(*args, **kwargs)
