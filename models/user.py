732f30c7fc76b6125fb31c6ee94a5f8785ae8df2
NikShiskobcki <nicolleshiskobcki@gmail.com>
2022-08-10 11:53:10 -0700

    fixes

--- models/user.py
+++ models/user.py
@@ -18,4 +18,4 @@ class User(BaseModel, Base):
     places = relationship("Place", cascade="all, delete-orphan",
                           backref='user')
     reviews = relationship("Review", cascade="all, delete-orphan",
-                           backref='user'))
+                           backref='user')
