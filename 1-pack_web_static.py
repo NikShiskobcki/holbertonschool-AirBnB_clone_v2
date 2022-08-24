b7012ada2bca4a9349b8e4a51ba5f253e826dd96
NikShiskobcki <nicolleshiskobcki@gmail.com>
2022-08-24 13:04:40 -0700

    task 1

---
+++ 1-pack_web_static.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Fabric script that generates.tgz archive from contents of web_static"""
+from datetime import datetime
+from fabric.api import run, local
+
+def do_pack():
+    """function for tgz archive"""
+    try:
+        local('mkdir -p versions')
+        time = datetime.now().strftime("%Y%m%d%H%M%S")
+        filepath = "versions/web_static_" + time + ".tgz"
+        local("tar -cvzf" + filepath + " web_static")
+        return filepath
+    except:
+        return None
