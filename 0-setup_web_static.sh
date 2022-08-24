2d2a1fd42457979e2d37eded5d2612885e355f5e
NikShiskobcki <nicolleshiskobcki@gmail.com>
2022-08-24 11:20:54 -0700

    task =

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,31 +2,30 @@
 # setting up web servers for the deployment of web_static
 
 #installs nginx
-sudo su
-apt-get update -y
-apt-get install nginx -y
+sudo apt-get update -y
+sudo apt-get install nginx -y
 
 #creates directories needed
-mkdir -p /data/web_static/releases/test/
-mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
 
 # fake html
-printf %s "<html>
+echo "<html>
     <head>
     </head>
     <body>
-        test html
+        Holberton School
     </body
 </html>" > /data/web_static/releases/test/index.html
 
 #creating symbolic link
-ln -sf /data/web_static/releases/test/ /data/web_static/current/
+ln -sf /data/web_static/releases/test/ /data/web_static/current
 
 # give ownership to user and group
-chown -R ubuntu /data/
-chgrp -R ubuntu /data
+sudo chown -R ubuntu /data/
+sudo chgrp -R ubuntu /data
 
 #update nginx configuration
-sed -i "/listen 80 default_server/a location /hbnb_static/ { alias /data/web_static/current/;}" \etc/nginx/sites-available/default
+sed -i "/listen 80 default_server/a location /hbnb_static/ { alias /data/web_static/current/;}" /etc/nginx/sites-available/default
 
-sudo service nginx start
+sudo service nginx restart
