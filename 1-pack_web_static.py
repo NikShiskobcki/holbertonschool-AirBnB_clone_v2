f88844434910a7c1c5e72aab31fca72e60c574af
Axel Bouvier <98038816+AxelBouvierM@users.noreply.github.com>
2022-08-25 21:16:10 -0300

    Update 1-pack_web_static.py

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -2,15 +2,17 @@
 """script generates a tgz archive from the contents of the web_static folder"""
 from fabric.api import local
 from datetime import datetime
+from os.path import isdir
 
 
 def do_pack():
-    """Task 1"""
-    time = datetime.strftime(datetime.now(), "%Y%m%d%H%M%S")
-    local('mkdir -p versions')
-    file = 'versions/web_static_{}.tgz'.format(time)
-    file_ = local('tar -czvf {} web_static'.format(file))
-    if file_.failed:
+    """generates a tgz archive"""
+    try:
+        date = datetime.now().strftime("%Y%m%d%H%M%S")
+        if isdir("versions") is False:
+            local("mkdir versions")
+        file_name = "versions/web_static_{}.tgz".format(date)
+        local("tar -cvzf {} web_static".format(file_name))
+        return file_name
+    except Exception:
         return None
-    else:
-        return file
