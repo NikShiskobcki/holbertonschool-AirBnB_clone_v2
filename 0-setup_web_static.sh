5b72967c33af1bc6d6969c9b74abea6c59073d31
Axel Bouvier <98038816+AxelBouvierM@users.noreply.github.com>
2022-08-25 21:14:42 -0300

    Update 0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -17,6 +17,6 @@ echo "
 	</body>
 </html>" > /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-sudo chown -R ubuntu:ubuntu /data/
+sudo chown -hR ubuntu:ubuntu /data/
 sudo sed -i '/:80 default_server;/a \\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
 sudo service nginx restart
