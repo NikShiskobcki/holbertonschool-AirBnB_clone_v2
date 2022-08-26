5fbcfaba5e52581143f8e437991ae82fe27c5dd8
Axel Bouvier <98038816+AxelBouvierM@users.noreply.github.com>
2022-08-25 21:25:27 -0300

    Update 0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,22 +1,17 @@
 #!/usr/bin/env bash
-# Bash script that sets up your web servers for the deployment of web_static
-sudo apt-get update -y
+# Script that sets up a web server for the deployment of web_static
+sudo apt-get update -y && sudo apt upgrade -y
 sudo apt-get install nginx -y
-sudo mkdir -p /data/
-sudo mkdir -p /data/web_static/
-sudo mkdir -p /data/web_static/releases/
-sudo mkdir -p /data/web_static/shared/
-sudo mkdir -p /data/web_static/releases/test/
-sudo touch /data/web_static/releases/test/index.html
-echo "
-<html>
-	<head>
-	</head>
-    <body>
-		<p>Holberton School</p>
-	</body>
-</html>" > /data/web_static/releases/test/index.html
+if [ ! -d /data/web_static/shared ]
+then
+	sudo mkdir -p /data/web_static/shared
+fi
+if [ ! -d /data/web_static/releases/test ]
+then
+	sudo mkdir -p /data/web_static/releases/test
+fi
+printf "<html>\n\t<head>\n\t</head>\n\t<body>\n\t\tFirst Web Deploy\n\t</body>\n</html>\n" > /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-sudo chown -hR ubuntu:ubuntu /data/
-sudo sed -i '/:80 default_server;/a \\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
+sudo chown -hR ubuntu:ubuntu /data
+grep -qxF $'\tlocation /hbnb_static/' /etc/nginx/sites-available/default || sed -i '/^\tserver_name/ a\\n\tlocation \/hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
 sudo service nginx restart
