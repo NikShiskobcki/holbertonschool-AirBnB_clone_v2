c17a65fe6f0a688a379bc2f15310fa3b04a04c0e
NikShiskobcki <87676940+NikShiskobcki@users.noreply.github.com>
2022-09-03 14:43:53 -0300

    task 3

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -10,16 +10,19 @@ def hello():
     """say hello"""
     return "Hello HBNB!"
 
+
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
     """display hbnb"""
     return "HBNB"
+
 
 @app.route("/c/<text>", strict_slashes=False)
 def c(text):
     """display c"""
     text = text.replace("_", " ")
     return "C {}".format(text)
+
 
 @app.route("/python", strict_slashes=False)
 @app.route("/python/<text>", strict_slashes=False)
