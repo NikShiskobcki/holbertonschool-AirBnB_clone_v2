edaaa266bd6559a17b3aad6181ab97b5d279f9e8
NikShiskobcki <nicolleshiskobcki@gmail.com>
2022-09-03 11:22:57 -0700

    task 7

--- models/state.py
+++ models/state.py
@@ -12,16 +12,15 @@ class State(BaseModel, Base):
     """ State class """
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
-    if (getenv("HBNB_TYPE_STORAGE") == "db"):
-        cities = relationship('City', backref="state", cascade="all, \
-                delete-orphan")
-    else:
-        @property
-        def cities(self):
-            """list cities"""
-            from models import storage
-            mcities = []
-            for city in storage.all(City).values():
-                if city.state_id == self.id:
-                    mcities.append(models.storage.all(City)[city])
-            return mcities
+    cities = relationship('City', backref="state", cascade="all, \
+                          delete-orphan")
+
+    @property
+    def cities(self):
+        """list cities"""
+        from models import storage
+        mcities = []
+        for city in storage.all(City).values():
+            if city.state_id == self.id:
+                mcities.append(models.storage.all(City)[city])
+        return mcities
