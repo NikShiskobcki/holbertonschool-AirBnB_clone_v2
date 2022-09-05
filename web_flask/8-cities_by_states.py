e2bc63709c58d5113bb190b1e15a62abc9621c54
NikShiskobcki <nicolleshiskobcki@gmail.com>
2022-09-05 11:20:43 -0700

    task 9

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -21,19 +21,12 @@ def stateslist():
     statesList = sorted(statesList, key=lambda k: k.name)
     return render_template("7-states_list.html", statesList=statesList)
 
-
-@app.route("/states_list", strict_slashes=False)
-def citylist():
+@app.route("/cities_by_states", strict_slashes=False)
+def citiesState():
     """displays html"""
     statesList = storage.all(State).values()
     statesList = sorted(statesList, key=lambda k: k.name)
-    stateCities = []
-    for st in sts:
-        stateCities.append([st, sorted(state.cities, key=lambda k: k.name)])
-    return render_template("8-cities_by_states.html",
-                            sts=stateCities,
-                            hd="States")
-
+    return render_template("8-cities_by_states.html", statesList=statesList)
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000)
